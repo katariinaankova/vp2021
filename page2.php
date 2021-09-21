a08c808c06422d2e65d65f7b7ea6a1fc4d7c6f45
Katariina Ankova <katariinaankova@users.noreply.github.com>
2021-09-21 11:55:44 +0300

    neljas tund

--- page2.php
+++ page2.php
@@ -39,14 +39,17 @@
 
   $file_count = count($all_photos);
   $photo_num = mt_rand(0, $file_count - 1);
+  
+  
   //echo $photo_num;
   //<img src="photos/pilt.jpg" alt="Tallinna Ülikool">
   $photo_html = '<img src="' .$photo_dir .$all_photos[$photo_num] .'" alt="Tallinna Ülikool">';
   $photo_list_html = "\n <ul> \n";
+  $photo_file_html= "\n <p>".$all_photos[$photo_num]
   
   
   //tsükkel
-  //for($i=agväärtus; $i < piirväärtus; $i muutumine)(...)
+  //for($i=algväärtus; $i < piirväärtus; $i muutumine)(...)
 	  
   //<ul>
   //<li>pildifail.jpg</li>
@@ -54,16 +57,20 @@
   //<li>pildifail1.jpg</li>
   //</ul>
 	  
-  for($i = 0; $i < $file_count; $i ++)(
+  for($i = 0; $i < $file_count; $i ++){
      $photo_list_html .= "<li>" .$all_photos[$i] ."</li> \n";
-  )
+  }
   $photo_list_html .= "</ul> \n";
+  
   
   $photo_select_html = '<select name="photo_select">' ."\n";
   for($i = 0; $i < $file_count; $i ++){
-     $photo_select_html .= '<option value="' .$ .'">' .$all_photos[$i] ."</option> \n";
+     $photo_select_html .= '<option value="' .$i .'">' .$all_photos[$i]."</option> \n";
 	 }
-	 $photo_select_html .=
+	 $photo_select_html .= "</select \n";
+
+
+  
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -80,7 +87,7 @@
   <p>Sügis 2021</p>
   <hr>
   <!--ekraanivorm-->
-  <form method="Post">
+  <form method="post">
      <input type="text" name="todays_adjective_input" placeholder="Tänase päeva ilma omadus" value="<?php echo $todays_adjective; ?>">
 	 <input type="submit" name="submit_todays_adjective" value="Saada ära">
 	 <span><?php echo $adjective_error; ?></span>
@@ -90,7 +97,19 @@
   
   <form method="POST">
         <?php echo $photo_select_html; ?>
+		<input type="submit" name="submit_todays_adjective" value="Saada ära">
+	<select name="photo_select">
+	
+   <ul>
+   <li>tlu_astra_600x400_1.jpg</li>
+   <li>tlu_astra_600x400_2.jpg</li>
+   <li>tlu_astra_600x400_3.jpg</li>
+   <li>tlu_mare_2</li>
+   </ul>
   </form>
+  
+  
+	
   <?php echo $photo_html; 
         echo $photo_list_html;
   ?>
